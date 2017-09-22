a1c927534351154927c3477a91d73a1c6f9a4ea0
Xana8U <xana8u@gmail.com>
2017-09-22 17:35:22 +0300

    - css for index - html files - updated models - updated views - updated urls - updated application list

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'polls.apps.PollsConfig',
+    'Hakemus.apps.HakemusConfig',
     'chat.apps.ChatConfig',
     'django.contrib.admin',
     'django.contrib.auth',
